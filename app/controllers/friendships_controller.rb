39b5ceeeb18c3aacd729b0bcc652cc2ddcbf8dd2
Matthew Hummer <biggreyson@gmail.com>
2018-01-03 23:42:17 -0700

    delete friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -19,7 +19,19 @@ class FriendshipsController < ApplicationController
   end
 
   def delete
-    puts "delete friend!"
-    redirect_to root_path
+    return redirect_to root_path if not cookies.signed[:user]
+    id = JSON.parse(cookies.signed[:user])["id"].to_i
+    friend_id = params[:id].to_i
+
+    if id < friend_id then
+      lower = id
+      higher = friend_id
+    else
+      lower = friend_id
+      higher = id
+    end
+
+    friendship = Friendship.where({:friend_1 => lower, :friend_2 => higher}).first
+    redirect_back fallback_location: root_path if friendship.destroy
   end
 end
