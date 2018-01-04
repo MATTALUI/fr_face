3f08afe293dc2bf3e9629d1fa888378e7bb4aa15
Matthew Hummer <biggreyson@gmail.com>
2018-01-04 14:42:38 -0700

    render all friends' posts in the feed

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -4,6 +4,14 @@ class FeedController < ApplicationController
     id = JSON.parse(cookies.signed[:user])["id"]
     @user = User.find_by_id(id)
     @all_posts = []
-    puts User.friends(@user[:id])
+    @all_posts.concat(@user.posts)
+    my_friends = User.friends(@user[:id])
+    my_friends.each { |friend|
+      @all_posts.concat(friend.posts)
+    }
+    @all_posts.sort! {|pa, pb|
+      pa[:created_at] <=> pb[:created_at]
+    }
+    @all_posts.reverse!
   end
 end
